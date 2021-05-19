35d4376fe901adfcdc8be59f84f12bef5cf03cfa
Matt Watkins <matthew.watkins@network.rca.ac.uk>
2021-05-19 17:35:57 +0100

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -3,20 +3,27 @@ height = scrollbit.rows()
 width = scrollbit.cols()
 totalPixels = scrollbit.rows() * scrollbit.cols()
 
-def on_forever():
+
+# Function takes a numbered grid array of values and converts them into cartesion X and Y
+def ledPos(number: number):
+    # Calculate x value from the array number
+    xpos = (number - 1) % width
+    # Calculate y value from the array number
+    ypos = Math.round((number - 1) / width)
+    # return value as tuple
+    return [xpos, ypos]
+
+# Create a chaser of lights with a start point (head) and an end point (tail) using A button   
+def on_button_pressed_a():
     ledNumber = 0
-    offset = randint(0, 80)
+    offset = randint(0, 50)
     while ledNumber <= totalPixels + offset:
         ledNumber += 1
+        # Send grid number and use function to output x and y
         head = ledPos(ledNumber)
         tail = ledPos(ledNumber - offset)
+        # Assign tuple values to x and y to set pixel position in the Scroll:Bit. Set brightness randomly
         scrollbit.set_pixel(head[0], head[1], randint(0, 255))
         scrollbit.set_pixel(tail[0], tail[1], 0)
         scrollbit.show()
-basic.forever(on_forever)
-
-
-def ledPos(number: number):
-    xpos = (number - 1) % width
-    ypos = Math.round((number - 1) / width)
-    return [xpos, ypos]
+input.on_button_pressed(Button.A, on_button_pressed_a)
--- main.ts
+++ main.ts
@@ -2,23 +2,30 @@ let ledNumber = 0
 let height = scrollbit.rows()
 let width = scrollbit.cols()
 let totalPixels = scrollbit.rows() * scrollbit.cols()
-basic.forever(function on_forever() {
+//  Function takes a numbered grid array of values and converts them into cartesion X and Y
+function ledPos(number: number): number[] {
+    //  Calculate x value from the array number
+    let xpos = (number - 1) % width
+    //  Calculate y value from the array number
+    let ypos = Math.round((number - 1) / width)
+    //  return value as tuple
+    return [xpos, ypos]
+}
+
+//  Create a chaser of lights with a start point (head) and an end point (tail) using A button   
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     let head: number[];
     let tail: number[];
     let ledNumber = 0
-    let offset = randint(0, 80)
+    let offset = randint(0, 50)
     while (ledNumber <= totalPixels + offset) {
         ledNumber += 1
+        //  Send grid number and use function to output x and y
         head = ledPos(ledNumber)
         tail = ledPos(ledNumber - offset)
+        //  Assign tuple values to x and y to set pixel position in the Scroll:Bit. Set brightness randomly
         scrollbit.setPixel(head[0], head[1], randint(0, 255))
         scrollbit.setPixel(tail[0], tail[1], 0)
         scrollbit.show()
     }
 })
-function ledPos(number: number): number[] {
-    let xpos = (number - 1) % width
-    let ypos = Math.round((number - 1) / width)
-    return [xpos, ypos]
-}
-
